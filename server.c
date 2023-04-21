af9aec6e073dcc427e733db30d434c4ec8efb170
zesilva_18 <zepetotiburso@gmail.com>
2023-04-21 16:52:41 +0100

    serverudpdone

--- server.c
+++ server.c
@@ -4,6 +4,9 @@ int shmid;
 shared_memory *shm;
 sem_t *semshmid;
 
+int PORT_CONFIG; // colocar 9876 --concexao udp
+int PORT_NOTICIAS; //colocar 9000
+
 void add_user(const char *name, const char *password, const char *role) {
     user *new_user = malloc(sizeof(user));
     if (new_user == NULL) {
@@ -58,9 +61,7 @@ int find_user(const char *name) {
     }
     return 0;
 }
-
 //funtion to search a user name and password, see if role == administrator and return boolean
-
 bool loginCheck(const char *name, const char *password) {
     user *current = shm->head;
     while (current != NULL) {
@@ -118,7 +119,8 @@ void erro(char *s) {
     exit(1);
 }
 
-int main(void) {
+int udpConextion() {
+
     int sockfd;
 
     struct sockaddr_in si_minha, si_outra, dest_addr;
@@ -127,27 +129,6 @@ int main(void) {
     socklen_t slen = sizeof(si_outra);
     char buf[BUFLEN];
 
-    // init shared memory
-    if ((shmid = shmget(IPC_PRIVATE, sizeof(shared_memory) + sizeof(user) * 20, IPC_CREAT | 0777)) < 0) {
-        perror("shmget");
-        exit(1);
-    }
-
-    // attach shared memory
-    if ((shm = (shared_memory *)shmat(shmid, NULL, 0)) == (shared_memory *)-1) {
-        perror("shmat");
-        exit(1);
-    }
-
-    // Inicializa o semáforo
-    if ((semshmid = sem_open("SEM_SHM", O_CREAT, 0777, 1)) == SEM_FAILED) {
-        perror("sem_open");
-        exit(1);
-    }
-
-    getconfig("config.txt");
-
-    printSharedMemory();
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
@@ -159,7 +140,6 @@ int main(void) {
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
         erro("Erro na criação do socket");
     }
-    // printf("\nMenu:\nADD_USER {username} {password}\nDEL {username}\nLIST\nQUIT\nQUIT_SERVER\n\n");
     printf("Bem vindo ao servidor\n");
 
     memset(&dest_addr, 0, sizeof(dest_addr));
@@ -179,7 +159,9 @@ int main(void) {
 
     bool login = false;
 
-    while (1) {
+    
+
+        while (1) {
         // printf("\nMenu\nADD_USER {username} {password} {role}\nDEL {username}\nLIST\nQUIT\nQUIT_SERVER\n\n",inet_ntoa(si_outra.sin_addr), ntohs(si_outra.sin_port));
 
         // Espera recepção de mensagem (a chamada é bloqueante)
@@ -204,7 +186,7 @@ int main(void) {
             }
             sendto(s, "Escreva username password: \n", strlen("Escreva username password: \n"), 0, (struct sockaddr *)&si_outra, slen);
             //print aux
-            printf("aux = %d\n", aux);
+            //printf("aux = %d\n", aux);
             if(aux == 2){
 
                 bufInstructions[1][strlen(bufInstructions[1]) - 1] = '\0';
@@ -218,6 +200,8 @@ int main(void) {
                     sendto(s, "Login errado\n", strlen("Login errado\n"), 0, (struct sockaddr *)&si_outra, slen);
                 }
             }
+
+            //sendto(s, "Escreva username password: \n", strlen("Escreva username password: \n"), 0, (struct sockaddr *)&si_outra, slen);
         }
         
         
@@ -232,13 +216,16 @@ int main(void) {
 
             if (strcmp(bufInstructions[0], "ADD_USER") == 0) {
                 // printf("aux = %d", aux);
-                if (aux == 4) {
+
+                bufInstructions[3][strlen(bufInstructions[3]) - 1] = '\0';
+
+                if (aux == 4 && strcmp(bufInstructions[3], "leitor") == 0 || strcmp(bufInstructions[3], "escritor") == 0 || strcmp(bufInstructions[3], "admin") == 0) {
                     printf("Adicionando um novo user com o nome %s, password %s role %s\n", bufInstructions[1], bufInstructions[2], bufInstructions[3]);
-                    bufInstructions[3][strlen(bufInstructions[3]) - 1] = '\0';
                     add_user(bufInstructions[1], bufInstructions[2], bufInstructions[3]);
                     sendto(s, "User adicionado com sucesso\n", strlen("User adicionado com sucesso\n"), 0, (struct sockaddr *)&si_outra, slen);
                 } else {
-                    printf("ERRO!!!\nUtilize o comando (ADD_USER) com a seguinte formatação ---> ADD_USER {username} {password}\n");
+                    sendto(s, "ERRO!!!\nUtilize o comando (ADD_USER) com o seguinte formato: ADD_USER {username} {password} {role}\n", strlen("ERRO!!!\nUtilize o comando (ADD_USER) com o seguinte formato: ADD_USER {username} {password} {role}\n"), 0, (struct sockaddr *)&si_outra, slen);
+                    sendto(s, "De relembrar que um user so pode ter role de leitor, escritor ou admin\n", strlen("De relembrar que um user so pode ter role de leitor, escritor ou admin\n"), 0, (struct sockaddr *)&si_outra, slen);
                 }
             } else if (strcmp(bufInstructions[0], "DEL") == 0) {
                 // printf("aux = %d", aux);
@@ -255,7 +242,7 @@ int main(void) {
                     }
 
                 } else {
-                    printf("ERRO!!!\nUtilize o comando (DEL) com a seguinte formatação ---> DEL {username}\n");
+                    sendto(s, "ERRO!!!\nUtilize o comando (DEL) com o seguinte formato: DEL {username}\n", strlen("ERRO!!!\nUtilize o comando (DEL) com o seguinte formato: DEL {username}\n"), 0, (struct sockaddr *)&si_outra, slen);
                 }
             } else if (strcmp(bufInstructions[0], "LIST\n") == 0) {
                 // printf("aux = %d\n", aux);
@@ -275,12 +262,13 @@ int main(void) {
                         aux = aux->next;
                     }
                 } else {
-                    printf("ERRO!!!\nUtilize o comando (LIST) com a seguinte formatação ---> LIST\n");
+                    sendto(s, "ERRO!!!\nUtilize o comando (LIST) com a seguinte formatação ---> LIST\n", strlen("ERRO!!!\nUtilize o comando (LIST) com a seguinte formatação ---> LIST\n"), 0, (struct sockaddr *)&si_outra, slen);
                 }
             } else if (strcmp(bufInstructions[0], "QUIT\n") == 0) {
                 // printf("aux = %d", aux);
                 if (aux == 1) {
                     printf("Desligando do cliente\n");
+                    sendto(s, "Desligando do cliente\n", strlen("Desligando do cliente\n"), 0, (struct sockaddr *)&si_outra, slen);
                     login = false;
                 } else {
                     printf("ERRO!!!\nUtilize o comando (QUIT) com a seguinte formatação ---> QUIT\n");
@@ -306,4 +294,60 @@ int main(void) {
     // Fecha socket e termina programa
     close(s);
     close(sockfd);
+
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 4) {
+        printf("numero de argumentos invalido!\n");
+        printf("./news_server <PORTO_NOTICIAS> <PORTO_CONFIG> <ficheiro configuração>!\n");
+        exit(1);
+    }
+
+    PORT_NOTICIAS = (int) strtol(argv[1], NULL, 10);
+    PORT_CONFIG = (int) strtol(argv[2], NULL, 10);
+
+    //convert argv[3] to a string 
+
+    char *configfile = argv[3];
+
+    //print configfile
+
+    //printf("configfile: %s \n", configfile);
+
+    //check if configfile is "config.txt" if not exit
+
+    if (strcmp(configfile, "config.txt") != 0) {
+        printf("ficheiro de configuração invalido!\n");
+        exit(1);
+    }
+
+        // init shared memory
+    if ((shmid = shmget(IPC_PRIVATE, sizeof(shared_memory) + sizeof(user) * 20, IPC_CREAT | 0777)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+
+    // attach shared memory
+    if ((shm = (shared_memory *)shmat(shmid, NULL, 0)) == (shared_memory *)-1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    // Inicializa o semáforo
+    if ((semshmid = sem_open("SEM_SHM", O_CREAT, 0777, 1)) == SEM_FAILED) {
+        perror("sem_open");
+        exit(1);
+    }
+
+    
+    getconfig("config.txt");
+
+    printSharedMemory();
+
+    udpConextion();
+
+    return 0;
+
 }
